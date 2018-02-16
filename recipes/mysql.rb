4393b3d86304657577d3ee4d6bfcfe0299acbb21
Nghiem Ba Hieu <hi3unb@gmail.com>
2018-02-17 01:40:38 +0700

    mysql service listens locally by default

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -8,9 +8,12 @@ mysql2_chef_gem 'default' do
   action :install
 end
 
+bind_address = node['mysql']['bind_address'] || '127.0.0.1'
+
 mysql_service 'default' do
   port '3306'
   version '5.7'
+  bind_address bind_address
   initial_root_password node['mysql']['server_root_password'] || 'change me'
   action [:create, :start]
 end
